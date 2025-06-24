fa66b6b8e6c799dbf3a4b0bafed2688203390f8d
[dyad] <git@dyad.sh>
2025-06-24 16:21:20 +0200

    [dyad] Created missing data file for CantiereForm - wrote 1 file(s)

---
+++ src/lib/cantiere-data.ts
@@ -0,0 +1,74 @@
+export const addettiList: string[] = [
+  "639 ABBATE EMANUELE",
+  "2 GRASSO DAVIDE",
+  "14 BALISTRERI GIOVANNI",
+  "25 CARUSO GIUSEPPE",
+  "30 DI GAETANO MARIO",
+  "45 FANARA ANTONIO",
+  "50 GAMBINO LUCA",
+  "60 GUASTELLA MARCO",
+  "75 MESSINA ANDREA",
+  "80 MINARDI PAOLO",
+  "90 PALMERI FRANCESCO",
+  "100 RAFFAELE GIUSEPPE",
+];
+
+export const servizioOptions: string[] = [
+  "Guardia Particolare Giurata",
+  "Addetto Servizi Fiduciari",
+  "Addetto Servizi Fiduciari con Auto",
+  "Servizio di Piantonamento",
+  "Servizio di Ronda",
+  "Servizio di Scorta Valori",
+  "Servizio di Vigilanza Armata",
+  "Servizio di Portierato",
+];
+
+export const clienteOptions: { codice: string; ragioneSociale: string }[] = [
+  { codice: "CLI001", ragioneSociale: "AEROVIAGGI SPA" },
+  { codice: "CLI002", ragioneSociale: "COSEDIL SPA" },
+  { codice: "CLI003", ragioneSociale: "F.LLI MAMMANA SRL" },
+  { codice: "CLI004", ragioneSociale: "SICUREZZA PRO S.R.L." },
+  { codice: "CLI005", ragioneSociale: "EDILIZIA FUTURA S.P.A." },
+];
+
+export const tipologiaAutomezzoOptions: string[] = [
+  "Auto",
+  "Furgone",
+  "Moto",
+  "Furgone Blindato",
+  "Auto Blindata",
+];
+
+export const marcaAutomezzoOptions: string[] = [
+  "Fiat",
+  "Ford",
+  "Mercedes-Benz",
+  "BMW",
+  "Audi",
+  "Volkswagen",
+  "Iveco",
+  "Renault",
+];
+
+export const tipologiaAttrezzoOptions: string[] = [
+  "Radio Portatile",
+  "Torcia",
+  "Estintore",
+  "Kit Primo Soccorso",
+  "Giubbotto Antiproiettile",
+  "Casco",
+  "Telecamera Portatile",
+  "DPI Vari",
+];
+
+export const marcaAttrezzoOptions: string[] = [
+  "Motorola",
+  "Hytera",
+  "Maglite",
+  "Streamlight",
+  "ABC",
+  "MSA",
+  "3M",
+  "GoPro",
+];
