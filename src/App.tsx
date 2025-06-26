022359f7ca4bca0d5f7dda926441c2c1d4eb385f
[dyad] <git@dyad.sh>
2025-06-26 11:37:10 +0200

    [dyad] Correzione dell'importazione del componente DotazioniDiServizio in App.tsx. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,40 @@
-import { DotazioniDiServizio } from "@/pages/DotazioniDiServizio";
-// ... altri import
+import DotazioniDiServizio from "@/pages/DotazioniDiServizio";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import DashboardLayout from "@/components/layout/DashboardLayout";
+import ServiceRequest from "@/pages/ServiceRequest";
+import Anagrafiche from "@/pages/Anagrafiche";
+import ServiceList from "@/pages/ServiceList";
+import RegistroDiCantiere from "@/pages/RegistroDiCantiere";
+import CentraleOperativa from "@/pages/CentraleOperativa";
+import ServiziCanone from "@/pages/ServiziCanone";
+import NotFound from "@/pages/NotFound";
+
+const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <Sonner />
       <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <Routes>
           <Route path="/" element={<DashboardLayout />}>
-            {/* ... altre routes */}
+            <Route index element={<CentraleOperativa />} /> {/* Changed default route to CentraleOperativa */}
+            <Route path="service-request" element={<ServiceRequest />} />
+            <Route path="anagrafiche" element={<Anagrafiche />} />
             <Route path="dotazioni-di-servizio" element={<DotazioniDiServizio />} />
-            {/* ... altre routes */}
+            <Route path="service-list" element={<ServiceList />} />
+            <Route path="registro-di-cantiere" element={<RegistroDiCantiere />} />
+            <Route path="centrale-operativa" element={<CentraleOperativa />} />
+            <Route path="servizi-canone" element={<ServiziCanone />} />
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
+
+export default App;
