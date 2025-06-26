c0ce3b55285f062eeabbd31d0b9b696c9b66c30b
[dyad] <git@dyad.sh>
2025-06-26 11:50:50 +0200

    [dyad] wrote 1 file(s)

---
+++ src/lib/dotazioni-data.ts
@@ -0,0 +1,73 @@
+export const serviceLocationOptions: string[] = [
+  "Agrigento - Segmento Portierato",
+  "Agrigento - Segmento VIGILANZA",
+  "Caltanissetta - Segmento Portierato",
+  "Caltanissetta - Segmento VIGILANZA",
+  "Catania - Segmento Portierato",
+  "Catania - Segmento VIGILANZA",
+  "Enna - Segmento Portierato",
+  "Enna - Segmento VIGILANZA",
+  "Messina - Segmento Portierato",
+  "Messina - Segmento VIGILANZA",
+  "Palermo - Segmento Portierato",
+  "Palermo - Segmento VIGILANZA",
+  "Ragusa - Segmento Portierato",
+  "Ragusa - Segmento VIGILANZA",
+  "Siracusa - Segmento Portierato",
+  "Siracusa - Segmento VIGILANZA",
+  "Trapani - Segmento Portierato",
+  "Trapani - Segmento VIGILANZA",
+];
+
+export const serviceTypeOptions: string[] = [
+  "G.P.G.",
+  "A.S.F.",
+];
+
+export const vehicleMakeModelOptions: string[] = [
+  "BETA 10", "BETA 11", "BETA 12", "BETA 13", "BETA 14", "BETA 15", "BETA 16",
+  "BETA 17", "BETA 18", "BETA 19", "BETA 20", "BETA 21", "BETA 22", "BETA 23",
+  "BETA 24", "BETA 25", "BETA 26", "BETA 27", "BETA 28", "BETA 29", "BETA 30",
+  "BETA 31", "BETA 32", "BETA 33", "BETA 34", "BETA 35", "BETA 36", "BETA 37",
+  "BETA 38", "BETA 39", "BETA 40", "BETA 41", "BETA 42", "BETA 43", "BETA 44",
+  "BETA 45", "BETA 46", "BETA 47", "BETA 48", "BETA 49", "BETA 50", "BETA 51",
+  "BETA 52", "BETA 53", "BETA 54", "BETA 55", "BETA 56", "BETA 57", "BETA 58",
+  "BETA 59", "BETA 60", "BETA 61", "BETA 62", "BETA 63", "BETA 64", "BETA 65",
+  "BETA 66", "BETA 67", "BETA 68", "BETA 69", "BETA 70", "BETA 71", "BETA 72",
+  "BETA 73", "BETA 74", "BETA 75", "BETA 76", "BETA 77", "BETA 78", "BETA 79",
+  "BETA 80", "BETA 81", "BETA 82", "BETA 83", "BETA 84", "BETA 85", "BETA 86",
+  "BETA 87", "BETA 88", "BETA 89", "BETA 90", "BETA 91", "BETA 92", "BETA 93",
+  "BETA 94", "BETA 95", "BETA 96", "BETA 97", "BETA 98", "BETA 99", "BETA 100",
+];
+
+export const vehiclePlateOptions: string[] = [
+  "GB317FW", "DH484KW", "EL438RT", "EF968TN", "EV662DB", "EV131XY", "FD133CT",
+  "GB314FW", "GB315FW", "GB316FW", "EG885KT", "GC657WR", "GC658WR", "GK262JP",
+  "GK264JP", "DC084FS", "DD064XN", "GK664JP", "FZ088GC", "FG127JY", "GK783JP",
+  "FM693KX", "FF027CC", "FF076LE", "FF029LE", "FR736GC", "FF041CC", "GK784JP",
+  "GK263JP", "GK785JP", "CV054ER",
+];
+
+export const vehicleInitialStateOptions: string[] = [
+  "SCARSO",
+  "SUFFICIENTE",
+  "BUONO",
+  "OTTIMO",
+];
+
+export const bodyworkDamageOptions: string[] = [
+  "NESSUNO",
+  "CARROZZERIA",
+  "CRISTALLI",
+  "GOMME",
+  "MOTORE",
+  "ALTRO (specificare in anomalie)",
+];
+
+export const employeeNameOptions: string[] = [
+  "AGLIECO", "ARTINO", "BALISTRERI", "CACCAVELLI", "CARUSO G", "CASA'",
+  "CASTIGLIONE", "COSTANTINO", "DI GAETANO M", "DI GAETANO S", "FANARA",
+  "GAMBINO", "GRANATA", "GRASSO", "GUASTELLA M", "GUASTELLA S", "GULLOTTA",
+  "GUTTADAURO", "MESSANA", "MESSINA A", "MESSINA D", "MINARDI", "MOTTA",
+  "PALMERI", "PASSARE", "RAFFAELE", "SANA", "SCANDURRA", "SENA", "TOSCANO", "VASTA"
+];
