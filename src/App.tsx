c1e7006338c73acb4901138d31d78a04fb8eeb2a
[dyad] <git@dyad.sh>
2025-06-26 11:36:22 +0200

    [dyad] wrote 3 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DashboardLayout from "./components/layout/DashboardLayout";
-import DashboardOverview from "./pages/DashboardOverview";
-import NotFound from "./pages/NotFound";
-import ServiceRequest from "./pages/ServiceRequest";
-import Anagrafiche from "./pages/Anagrafiche";
-import DotazioniDiServizio from "./pages/DotazioniDiServizio";
-import ServiceList from "./pages/ServiceList";
-import RegistroDiCantiere from "./pages/RegistroDiCantiere";
-import CentraleOperativa from "./pages/CentraleOperativa";
-import ServiziCanone from "./pages/ServiziCanone";
-
-const queryClient = new QueryClient();
+import { DotazioniDiServizio } from "@/pages/DotazioniDiServizio";
+// ... altri import
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -24,21 +9,12 @@ const App = () => (
       <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <Routes>
           <Route path="/" element={<DashboardLayout />}>
-            <Route index element={<CentraleOperativa />} />
-            <Route path="service-request" element={<ServiceRequest />} />
-            <Route path="anagrafiche" element={<Anagrafiche />} />
+            {/* ... altre routes */}
             <Route path="dotazioni-di-servizio" element={<DotazioniDiServizio />} />
-            <Route path="service-list" element={<ServiceList />} />
-            <Route path="registro-di-cantiere" element={<RegistroDiCantiere />} />
-            <Route path="centrale-operativa" element={<CentraleOperativa />} />
-            <Route path="servizi-a-canone" element={<ServiziCanone />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
+            {/* ... altre routes */}
           </Route>
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
-
-export default App;
