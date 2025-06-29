ba807a923a2f5619395d776e7486d1950b36f6bb
[dyad] <git@dyad.sh>
2025-06-29 14:22:18 +0200

    [dyad] Risolto l'errore di esportazione del componente DotazioniDiServizio. - wrote 1 file(s)

--- src/pages/DotazioniDiServizio.tsx
+++ src/pages/DotazioniDiServizio.tsx
@@ -1,2 +1,21 @@
-// Modifica l'import nel file che usa il componente
-import { ServiceReportForm } from "@/components/dotazioni-di-servizio/ServiceReportForm";
+import React from "react";
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
+import ServiceReportForm from "@/components/dotazioni-di-servizio/ServiceReportForm"; // Importa il componente con export default
+
+const DotazioniDiServizio = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <Card className="w-full max-w-4xl mx-auto">
+        <CardHeader>
+          <CardTitle className="text-3xl font-bold text-center">Rapporto Dotazioni di Servizio</CardTitle>
+          <CardDescription className="text-center">Compila il rapporto giornaliero sulle dotazioni di servizio.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ServiceReportForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default DotazioniDiServizio;
